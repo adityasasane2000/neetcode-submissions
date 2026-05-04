b38d9cffffb680c907714d3da26ac90875977734
Aditya Sasane <113760508+adityasasane2000@users.noreply.github.com>
2026-05-04 16:20:28 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] freq = new int[26];
+
+        for(int i=0;i<s.length();i++){
+            freq[s.charAt(i) - 'a']++;
+        }
+
+        for(int j=0;j<t.length();j++){
+            freq[t.charAt(j) - 'a']--;
+        }
+
+        for(int i=0;i<26;i++){
+            if(freq[i] > 0 || freq[i] < 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
